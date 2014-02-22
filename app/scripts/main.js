78260302c62ed1d1c06eaad0899f33e28d3e905b
Matt Bertino <mattbertino@gmail.com>
2014-02-22 18:48:06 -0500

    finalized mock-up except for product description

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+	$('.header').on('click','.js-shop', function(){
+		console.log('click')
+		$('.landing-center').toggleClass('hidden')
+		$('.product-gallery').toggleClass('hidden')
+	})
+
+	$('.product-gallery').on('click','.js-prod-unit', function(){
+		console.log('click')
+		$('.product-gallery').toggleClass('hidden')
+		$('.product-page').toggleClass('hidden')
+	})
+
+})
